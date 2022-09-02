cc44bd258890aab64c64fc9a6377cfd40e857041
user <emiljoji27@gmail.com>
2022-09-02 20:28:42 +0530

    location change

---
+++ KeeVault1/website/forms.py
@@ -0,0 +1,41 @@
+from asyncio.windows_events import NULL
+from pickle import FALSE
+from django import forms
+# from .models import PostsCreation
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.validators import UnicodeUsernameValidator
+
+from .models import PasswordModel
+username_validator = UnicodeUsernameValidator()
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(label=_('Username'),max_length=150,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[username_validator],
+        error_messages={'unique': _("Username already exists.")},
+        widget=forms.TextInput(attrs={'class': 'form-control','placeholder': 'Search'})
+    )
+
+    password1 = forms.CharField(label=_("Password"), strip=False, widget=forms.PasswordInput(
+        attrs={'autocomplete': 'new-password', 'class': "form-control form-control-lg",'placeholder': 'Search'}))
+
+    password2 = forms.CharField(label=_("Confirm Password"), widget=forms.PasswordInput(
+        attrs={'autocomplete': 'new-password', 'class': "form-control form-control-lg",'placeholder': 'Search'}), 
+        strip=False, help_text=_("Enter the same password as before, for verification."))
+    
+    email = forms.EmailField(label=_('Email'), widget=forms.TextInput(
+        attrs={'class': "form-control form-control-lg",'placeholder': 'Search'}))
+
+    class Meta:
+        model = User
+        fields = ['email', 'username']
+
+
+class PasswordEntryForm(forms.ModelForm):
+    class Meta:
+        model = PasswordModel
+        fields = ['name','login_url','login_username','login_password']
+
---
+++ KeeVault1/website/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path,include
+from . import views
+
+#app_name='website'
+urlpatterns = [
+    path('',views.index,name='home'),
+    path('login/',views.login,name='log_in'),
+    path('register/',views.register,name='register'),
+    # path('profile/',views.profile,name='profile'),
+    path('logout/',views.logout,name='log_out'),
+    path('generator/',views.generator,name='generator'),
+    path('dashboard/',views.dashboard,name='dashboard'),
+    path('new_entry/',views.new_entry,name='new_entry'),
+    path('view_details/',views.anonymous,name='anony'),
+    path(r'^delete/(?P<pk>[0-9]+)/$',views.delete_entry,name='delete_entry'),
+    path("password_reset/", views.password_reset_request, name='password__reset'),   
+]
