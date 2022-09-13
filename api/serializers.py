d03d687fdc06fb288ff6983c8988ea3aed51419c
unknown <emiljoji27@gmail.com>
2022-09-13 23:16:16 +0530

    post request to user

--- api/serializers.py
+++ api/serializers.py
@@ -34,7 +34,9 @@ class PasswordSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = super().create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
         return user
 
     class Meta:
